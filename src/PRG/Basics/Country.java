7e3cfd764d107f3672d97d5fd48cd74433341c08
Tomáš Hošek <tomas.hosek@infis.cz>
2026-09-08 13:20:47 +0200

    Změna výpisu

--- src/PRG/Basics/Country.java
+++ src/PRG/Basics/Country.java
@@ -3,20 +3,28 @@ package PRG.Basics;
 import fileworks.DataImport;
 
 public class Country {
-    public Country(String name, String continent, Long population, Double avgAge) {
+
+    String name, continent;
+    long population;
+    double avgAge;
+
+    public Country(String name, String continent, long population, double avgAge) {
+
+        this.name = name;
+        this.continent = continent;
+        this.population = population;
+        this.avgAge = avgAge;
     }
 
     public static void main(String[] args) {
 
         String path = "data/countries.txt";
-
         DataImport di = new DataImport(path);
 
-        Country czechia = new Country("Czech Republic", "Europe", 10_800_000L, 85.0);
+        Country czechia = new Country("Czech Republic", "Europe", 10_800_000, 85.0);
 
         String line = di.readLine();
         String[] tokeny = line.split(";");
-
         String name = tokeny[0];
         String continent = tokeny[1];
         long population = Long.parseLong(tokeny[2]);
@@ -24,8 +32,11 @@ public class Country {
 
         Country oneCountry = new Country(name, continent, population, avgAge);
 
-        System.out.println(oneCountry);
-
         di.finishImport();
+    }
+
+    public String toString() {
+
+        return name;
     }
 }
--- src/PRG/Basics/ParseSimple.java
+++ src/PRG/Basics/ParseSimple.java
@@ -3,12 +3,6 @@ package PRG.Basics;
 public class ParseSimple {
     public static void main(String[] args) {
 
-        String data = "This is a number:7";
 
-        String[] tokeny = data.split(":");
-
-        int parsedNumber = Integer.parseInt(tokeny[1]);
-
-        System.out.println(parsedNumber);
     }
 }
