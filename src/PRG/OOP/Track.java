f3d0e760a14817c0a10d53a08435b131e3e87024
Tomáš Hošek <tomas.hosek@infis.cz>
2026-09-15 13:20:57 +0200

    Změna výpisu

--- src/PRG/OOP/Track.java
+++ src/PRG/OOP/Track.java
@@ -1,5 +1,9 @@
 package PRG.OOP;
 
+import fileworks.DataImport;
+
+import java.util.ArrayList;
+
 public class Track {
 
     String nazev;
@@ -25,15 +29,49 @@ public class Track {
                 '}';
     }
 
-    public void setDelka(int delka) {
+    public String getNazev() {
+        return nazev;
+    }
 
-        if (delka < 1) {
+    public int getRok() {
+        return rok;
+    }
+
+    public double getHodnoceni() {
+        return hodnoceni;
+    }
+
+    public int getDelka() {
+        return delka;
+    }
+
+    public static void main(String[] args) {
+
+        DataImport di = new DataImport("data/tracks.txt");
+
+        ArrayList<Track> list = new ArrayList<>();
+
+        while (di.hasNext()) {
+
+            String radek = di.readLine();
+
+            String[] tokeny = radek.split(";");
+
+            Track song = new Track(tokeny[0], Integer.parseInt(tokeny[1]), Double.parseDouble(tokeny[2]), Integer.parseInt(tokeny[3]));
+
+            list.add(song);
+        }
+    }
+
+    public void setRok(int rok) {
+
+        if (rok < 0) {
 
             System.out.println("Neplatná hodnota");
             return;
         }
 
-        this.delka = delka;
+        this.rok = rok;
     }
 
     public void setHodnoceni(double hodnoceni) {
@@ -45,5 +83,16 @@ public class Track {
         }
 
         this.hodnoceni = hodnoceni;
+    }
+
+    public void setDelka(int delka) {
+
+        if (delka < 1) {
+
+            System.out.println("Neplatná hodnota");
+            return;
+        }
+
+        this.delka = delka;
     }
 }
